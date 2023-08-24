7638f290224546ffe95e2a596adb0e67dbda9c1f
Muhammad Abdul Hafiz <jay@mjah.xyz>
2023-08-24 22:15:30 +0100

    VELZ-651 Configure timezone, add Postgres driver, add application properties

--- src/main/java/com/velz/service/core/CoreApplication.java
+++ src/main/java/com/velz/service/core/CoreApplication.java
@@ -1,13 +1,24 @@
 package com.velz.service.core;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CoreApplication {
+
+    @Value("${server.time_zone}")
+    private String timeZone;
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+    }
 }
