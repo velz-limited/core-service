7fb56893bfee466241942d3ebc129626a326bae7
Muhammad Abdul Hafiz <jay@mjah.xyz>
2023-09-15 13:29:23 +0100

    VELZ-655 Add initial authorisation changes

--- src/main/java/com/velz/service/core/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/velz/service/core/configuration/security/SecurityConfiguration.java
@@ -2,11 +2,13 @@ package com.velz.service.core.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableMethodSecurity
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
--- src/main/java/com/velz/service/core/configuration/security/principal/UserRole.java
+++ src/main/java/com/velz/service/core/configuration/security/principal/UserRole.java
@@ -8,19 +8,28 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 @Getter
 @AllArgsConstructor
 public enum UserRole {
-    GUEST(PrefixedName.GUEST),
-    STANDARD(PrefixedName.STANDARD),
-    LOCKED(PrefixedName.LOCKED),
-    DELETED(PrefixedName.DELETED);
+    GUEST(PrefixedName.GUEST, PreAuthorize.GUEST),
+    STANDARD(PrefixedName.STANDARD, PreAuthorize.STANDARD),
+    LOCKED(PrefixedName.LOCKED, PreAuthorize.LOCKED),
+    DELETED(PrefixedName.DELETED, PreAuthorize.DELETED);
 
-    private interface PrefixedName {
+    public interface PrefixedName {
         String GUEST = "ROLE_GUEST";
         String STANDARD = "ROLE_STANDARD";
         String LOCKED = "ROLE_LOCKED";
         String DELETED = "ROLE_DELETED";
     }
 
+    public interface PreAuthorize {
+        String GUEST = "hasRole('" + PrefixedName.GUEST + "')";
+        String STANDARD = "hasRole('" + PrefixedName.STANDARD + "')";
+        String LOCKED = "hasRole('" + PrefixedName.LOCKED + "')";
+        String DELETED = "hasRole('" + PrefixedName.DELETED + "')";
+    }
+
     private final String prefixedName;
+
+    private final String preAuthorize;
 
     public GrantedAuthority getAuthority() {
         return new SimpleGrantedAuthority(this.getPrefixedName());
